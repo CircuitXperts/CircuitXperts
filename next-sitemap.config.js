f34af0622adbc2970abca33a07e4021ef7eb6e82
CircuitXperts <circuitxperts@gmail.com>
2024-08-24 01:58:21 +0530

    site map and build issue fixed

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.circuitxperts.com", // Replace with your domain
+  generateRobotsTxt: true, // (optional) Generate robots.txt
+  // ...other options
+};
